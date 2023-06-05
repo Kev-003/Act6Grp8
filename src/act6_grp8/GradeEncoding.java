476646db345342cb196e123d9aebdeb7f725d39e
hielp <hielp@MSI>
2023-06-05 11:17:37 +0800

    Change defaultCloseOperation

--- src/act6_grp8/GradeEncoding.java
+++ src/act6_grp8/GradeEncoding.java
@@ -77,7 +77,7 @@ class GradeEncoding extends javax.swing.JFrame {
         aquaticsRemark = new javax.swing.JLabel();
         nstpRemark = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setText("Grade Encoding");
